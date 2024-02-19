6f7ebf423a22401d279f14430291431e8082b8bf
bimus-github <bimus2022@gmail.com>
2024-02-19 16:32:07 +0500

    fixing

--- src/preload.ts
+++ src/preload.ts
@@ -1,39 +1,43 @@
 import { contextBridge } from "electron";
 import os from "os";
 import fs from "fs";
-import { BTree } from "./classes/BTree";
+import { BTreeWithObject } from "./classes/BTreeWithObject";
+import { Data_Type } from "./models";
 
 const dataPath = `${os.homedir()}/Users/sardorbekaminjonov/Madaminjon/electron-learning/electron-ts/data/data.json`;
 
-let initailData = {} as BTree;
-initailData = JSON.parse(fs.readFileSync(dataPath, "utf-8")) as BTree;
+let initailData = {} as BTreeWithObject;
+initailData = JSON.parse(fs.readFileSync(dataPath, "utf-8")) as BTreeWithObject;
 
 contextBridge.exposeInMainWorld("data", {
-  setData: async (data: number) => {
-    let btree = {} as BTree;
+  setData: async (d: Data_Type) => {
+    const tree = new BTreeWithObject(2);
 
-    btree = new BTree(4);
+    tree.root = initailData.root;
+    tree.t = initailData.t;
 
-    btree.root = initailData.root;
-    btree.t = initailData.t;
+    tree.insert(d);
 
-    btree.insert(data);
+    async function save() {
+      const json = JSON.stringify(tree, null, 2);
+      fs.writeFileSync(dataPath, json, "utf-8");
+    }
 
-    const asyncFunc = async () => {
-      fs.writeFileSync(dataPath, JSON.stringify(btree));
-    };
-
-    asyncFunc().then(() => {
-      initailData = JSON.parse(fs.readFileSync(dataPath, "utf-8")) as BTree;
+    await save().then(() => {
+      initailData = JSON.parse(
+        fs.readFileSync(dataPath, "utf-8")
+      ) as BTreeWithObject;
     });
   },
-  async getData() {
-    const data = initailData;
-    const tree = new BTree(4);
-    tree.root = data.root;
-    tree.t = data.t;
-    const array = await tree.getDataAsArray();
 
-    return array;
+  searchByProps: async (key: keyof Data_Type, value: string) => {
+    const tree = new BTreeWithObject(4);
+
+    tree.root = initailData.root;
+    tree.t = initailData.t;
+
+    console.log(tree);
+
+    return tree.searchByProps(key, value);
   },
 });
