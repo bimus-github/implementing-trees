16a4957da1f133b188ca6738ffac92c6ad97bba5
bimus-github <bimus2022@gmail.com>
2024-02-19 16:32:22 +0500

    fixing

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,19 +1,26 @@
-export type Data_Type = {
-  value: number;
-  right: Data_Type | null;
-  left: Data_Type | null;
-};
+// export type Data_Type = {
+//   value: number;
+//   right: Data_Type | null;
+//   left: Data_Type | null;
+// };
 
-export type Red_Black_Tree = {
-  data: number;
-  left: Red_Black_Tree | null;
-  right: Red_Black_Tree | null;
-  parent: Red_Black_Tree | null;
-  color: 0 | 1;
-};
+// export type Red_Black_Tree = {
+//   data: number;
+//   left: Red_Black_Tree | null;
+//   right: Red_Black_Tree | null;
+//   parent: Red_Black_Tree | null;
+//   color: 0 | 1;
+// };
 
-export type B_Tree_Type = {
-  children: B_Tree_Type[];
-  keys: number[];
+// export type B_Tree_Type = {
+//   children: B_Tree_Type[];
+//   keys: number[];
+//   leaf: boolean;
+// };
+
+export type Data_Type = { id: string; name: string; code: string };
+export type Node_Type = {
+  data: Data_Type[];
   leaf: boolean;
+  children: Node_Type[];
 };
